a182e4465df2eff3289e2adcaf4757542e7a2d73
= <=>
2025-04-26 23:11:13 -0300

    update versão framework

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'danieltm/validation-io',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'ab847aea735b6c8497be9f6acc3a79bcebfb64bc',
+        'reference' => '9ca04e2173543ccb9e22087e9aa810d4c7fc44fa',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'danieltm/origins' => array(
-            'pretty_version' => 'V1.6.1',
-            'version' => '1.6.1.0',
-            'reference' => 'cecc597d4cda1209fd31b4e19566ac058d3e92b4',
+            'pretty_version' => 'V2.0.0',
+            'version' => '2.0.0.0',
+            'reference' => '723509e561d7ead8702207b26041d7dfbe0eeb4b',
             'type' => 'library',
             'install_path' => __DIR__ . '/../danieltm/origins',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'danieltm/validation-io' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'ab847aea735b6c8497be9f6acc3a79bcebfb64bc',
+            'reference' => '9ca04e2173543ccb9e22087e9aa810d4c7fc44fa',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
